b1bc76430c6cf9421a8781e792026cc309143453
wilsacker <williamsuttona@gmail.com>
2024-09-16 16:40:18 -0700

    setting up api weather call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
+const weatherApiRootUrl = 'https://api.openweathermap.org';
 const weatherApiKey = "0aebe984c0fcc010def3e570e2cb9146"
 
 function fetchWeather() {
-    
+    let { lat } = location;
+    let { lon } = location;
+    const cityName = location.name;
+
+    const apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`
+
+    fetch(apiUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      renderItems(city, data);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
 }
